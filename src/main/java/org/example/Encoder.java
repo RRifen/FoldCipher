6974f71557ba2771e3245f215598ab6fc00d4a31
Arseny Bazhenov <gorbaty.kit@gmail.com>
2023-10-04 12:03:05 +0300

    Add rot Tests

---
+++ src/main/java/org/example/Encoder.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Encoder {
+    public List<String> ROT(List<String> strings, int offset) {
+        return new ArrayList<>();
+    }
+
+}
