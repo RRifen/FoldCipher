98d96a20bfc7afbd4a7143b5f6c6e308b1a4cdf9
Arseny Bazhenov <gorbaty.kit@gmail.com>
2023-10-04 12:12:24 +0300

    Add ROT realisation

--- src/main/java/org/example/Encoder.java
+++ src/main/java/org/example/Encoder.java
@@ -5,7 +5,16 @@ import java.util.List;
 
 public class Encoder {
     public List<String> ROT(List<String> strings, int offset) {
-        return new ArrayList<>();
+        if (offset < 0 || offset > 255) throw new IllegalArgumentException("Illegal argument #offset");
+        List<String> result = new ArrayList<>();
+        for(String line : strings) {
+            char[] res = new char[line.length()];
+            for(int i = 0; i < line.length(); i++) {
+                res[i] = (char) (line.charAt(i) + (char)offset);
+            }
+            result.add(String.valueOf(res));
+        }
+        return result;
     }
 
 }
